2e82d2f7bbfaa8f85e3951fcf5046dce992d402f
Soumya Ray <soumya.ray@gmail.com>
2015-09-21 06:50:13 +0800

    Added blocks example
    - logging example with and without blocks

---
+++ blocks.rb
@@ -0,0 +1,42 @@
+# LOGGING EXAMPLE
+def write_to_database(item)
+  puts "Starting writing to database"
+  result = DB.write(item)
+  puts "Completing writing to database"
+  result
+end
+
+def write_to_database(item_id)
+  puts "Starting reading from database"
+  item = DB.read(item_id)
+  puts "Completing reading from database"
+  item
+end
+
+
+# LOGGING WITH BLOCKS
+def with_logging(description)
+  puts "Starting #{description}"
+  value = yield
+  puts "Completing #{description}"
+  value
+end
+
+def write_to_database(item)
+  with_logging("database write") { DB.write(item) }
+end
+
+def read_from_database(item_id)
+  with_logging("database read") { DB.read(item_id) }
+end
+
+def read_from_cache(item_id)
+  with_logging("cache read") do
+    item = Cache.read(item_id)
+    unless result
+     item = DB.read(item_id)
+     Cache.write(item)
+    end
+    item
+  end
+end
