25cdff8a5a95efd3239017486ebd662a95b61e00
Soumya Ray <soumya.ray@gmail.com>
2021-09-26 16:55:23 +0800

    refactoring: correct ruby style

--- refactoring/name_play.rb
+++ refactoring/name_play.rb
@@ -43,3 +43,17 @@
 #   middle_initials = abbreviated(names[1..-2])
 #   [names.first, middle_initials, names.last].flatten.compact.join(' ')
 # end
+
+# 6. CORRECT RUBY STYLE!
+
+# # abbreviates names: takes array of names, returns string
+# def abbreviated(words)
+#   words.map { |word| "#{word[0]}." } unless words.empty?
+# end
+
+# # formalizes full name: takes string, returns string
+# def formal_name(full_name)
+#   names = full_name.split
+#   middle_initials = abbreviated(names[1..-2])
+#   [names.first, middle_initials, names.last].flatten.compact.join(' ')
+# end
