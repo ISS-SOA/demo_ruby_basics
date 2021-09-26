e7c8dd1792c91e0a775b3cf9d1aa145396012f5e
Soumya Ray <soumya.ray@gmail.com>
2021-09-26 16:48:50 +0800

    refactoring: full refactoring process

--- refactoring/name_play.rb
+++ refactoring/name_play.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
-# TESTS PASS BUT HARD TO READ
+# 1. NOTHING -- TESTS SHOULD FAIL
 
-def formal_name(full_name)
-  names = full_name.split
-  middle = names[1..names.length - 2]
+# 2. EMPTY METHOD -- TESTS SHOULD FAIL
 
-  if middle.count > 0
-    middle_names = middle.map { |mid| mid[0] + '.' }
-    [names.first, middle_names, names.last].join(' ')
-  else
-    full_name
-  end
-end
+# def formal_name(full_name)
+# end
 
-# CLEARER FLOW OF CODE
+# 3. TESTS PASS BUT HARD TO READ
+
+# def formal_name(full_name)
+#   names = full_name.split
+#   middle = names[1..names.length - 2]
+
+#   if middle.count > 0
+#     middle_names = middle.map { |mid| mid[0] + '.' }
+#     [names.first, middle_names, names.last].join(' ')
+#   else
+#     full_name
+#   end
+# end
+
+# 4. CLEARER FLOW OF CODE
 
 # def formal_name(full_name)
 #   names = full_name.split
@@ -23,7 +30,7 @@ end
 #   [names.first, middle_names, names.last].compact.join(' ')
 # end
 
-# WELL-NAMED METHODS
+# 5. WELL-NAMED METHODS
 
 # # abbreviates names: takes array of names, returns string
 # def abbreviated(words)
